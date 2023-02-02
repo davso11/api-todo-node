90ebfadabe8bb63bf5be210eb8354962cb56577f
David Sehi <david.sehi2016@gmail.com>
2023-02-02 14:12:54 +0000

    Going with the promise-based option

--- configs/mysqlConnection.js
+++ configs/mysqlConnection.js
@@ -1,7 +1,7 @@
-const mysql = require('mysql2')
+const mysql = require('mysql2/promise')
 
-module.exports = () => {
-  return mysql.createConnection({
+module.exports = async () => {
+  return await mysql.createConnection({
     host: process.env.MYSQL_HOST,
     user: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
