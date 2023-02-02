31809bdf54cef5d5f3f18b2fec61de2c8a5e546b
David Sehi <david.sehi2016@gmail.com>
2023-02-02 14:14:37 +0000

    Add `todos` routes

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
 const express = require('express')
 const cors = require('cors')
+const todoRoutes = require('./routes/todo-routes')
 
 const app = express()
 
 app.use(express.json())
 app.use(cors())
 
-app.use('/api/ping', (req, res) => {
-  res.status(200).json({ message: 'Got you !' })
-})
+app.use('/api/todos', todoRoutes)
 
 module.exports = app
