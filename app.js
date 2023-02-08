7c0457c39dd3acff976660dafd4e597c769d8f86
David Sehi <david.sehi2016@gmail.com>
2023-02-08 16:09:07 +0000

    Add middleware to parse URLEncoded data

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const todoRoutes = require('./routes/todo-routes')
 const app = express()
 
 app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
 app.use(cors())
 
 app.use('/api/todos', todoRoutes)
